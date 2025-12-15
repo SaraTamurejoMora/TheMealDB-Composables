a8e5320bf0bd47637beb9a01515502213efbca0d
Sara <sara.tamurejo.7e8@itb.cat>
2025-12-15 22:03:37 +0000

    Falta que funcione la busqueda

---
+++ TheMealDB/src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const categoriaMeal = ref(null)
+
+
+const emit = defineEmits(['buscarCategoriaMeal'])
+
+const buscarCategoriaMeal = () =>{
+    emit('buscarCategoriaMeal',categoriaMeal.value)
+}
+</script>
+
+
+<template>
+
+<div>
+    <input type="text" v-model="categoriaMeal" placeholder="Buscar categoria"/>
+    <button @click="buscarCategoriaMeal">Buscar</button>
+</div>
+
+
+</template>
--- TheMealDB/src/views/Home.vue
+++ TheMealDB/src/views/Home.vue
@@ -4,6 +4,7 @@ import { useFetch } from "@/composables/useFetch.js";
 import { RouterLink } from "vue-router";
 import MealCard from "@/components/MealCard.vue";
 import { ref } from "vue";
+import SearchBar from "@/components/SearchBar.vue";
 
 const cerca = ref("chicken");
 const url = ref(
@@ -11,12 +12,19 @@ const url = ref(
 );
 
 const { data, error, loading, fetchData } = useFetch(url);
+
+
+const buscarCategoriaMeal =(categoria)=>{
+    url.value = (`https://www.themealdb.com/api/json/v1/1/search.php?s=${categoria.value}`)
+}
+
 </script>
 
 <template>
   <div v-if="loading">Carregant...</div>
   <div v-else-if="error">{{ error }}</div>
   <div v-else>
+    <SearchBar @buscarCategoriaMeal="buscarCategoriaMeal"/>
     <ul v-if="data && data.meals">
       <div v-for="meal in data.meals" :key="meal.idMeal">
         <MealCard :id="meal.idMeal" :img="meal.strMealThumb" :name="meal.strMeal"/>
